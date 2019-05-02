9dc761b892e71f8f4e8cb2f1971c439be9276b33
Dylan Rodriguez <pikayou04@gmail.com>
2019-05-02 12:02:20 -0500

    initial commit

--- src/main/java/me/ohvalsgod/bedrock/player/profiling/callback/implementation/SimpleCallback.java
+++
@@ -1,9 +0,0 @@
-package me.ohvalsgod.bedrock.player.profiling.callback.implementation;
-
-import me.ohvalsgod.bedrock.player.profiling.callback.Callback;
-
-/**
- * A more simpler approach to the {@link Callback} class, that only takes in a value, with a result of {@link CommonResultType}
- * @param <V>
- */
-public interface SimpleCallback<V> extends Callback<V, CommonResultType> {}
--- src/main/java/me/ohvalsgod/bedrock/player/profiling/callback/result/ProfileCallbackResult.java
+++
@@ -1,7 +0,0 @@
-package me.ohvalsgod.bedrock.player.profiling.callback.result;
-
-public enum ProfileCallbackResult {
-
-    PROFILE_DOES_NOT_EXIST, STORAGE_IS_MISSING, PROFILE_SAVED_SUCCESSFULLY, PROFILE_LOADED_SUCCESSFULLY;
-
-}
